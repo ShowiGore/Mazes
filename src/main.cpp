bb0627df488933e60f88983c9ee44cb01e1401df
ShowiGore <david444488888888@gmil.com>
2025-11-24 18:30:25 +0100

    refactor: clean up main.cpp by removing unused constants and optimizing maze generation flow

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <time.h>
 #include <unistd.h>
-#include "constants.hpp"
 #include "utilities/TimeProfiler.hpp"
 #include "generators/Maze.hpp"
 #include "generators/RecursiveDivisionMaze.hpp"
@@ -9,30 +8,35 @@
 #include "generators/WilsonsMaze.hpp"
 
 int main() {
-    TimeProfiler tp;
-    tp.start();
+    constexpr int HEIGHT = 12345; //524288
+    constexpr int WIDTH = 12345; //524288
+    //std::random_device rd;
+    //const unsigned int seed = rd();
+    constexpr unsigned int seed = 0;
 
-    std::random_device rd;
-    const unsigned int seed = rd();
-    //constexpr unsigned int seed = 0;
+    TimeProfiler tp;
 
     std::cout << "Generating maze" << std::endl;
-    WilsonsMaze m(HEIGHT, WIDTH, seed);
 
-    //std::cout << "Saving maze" << std::endl;
-    //m.save_maze();
+    tp.start();
+    WilsonsMaze m(HEIGHT, WIDTH, seed);
+    tp.stop();
+    tp.print();
 
     //m.print();
     //m.printSimple();
+    std::cout << "Saving maze" << std::endl;
+    m.save_maze();
+
     std::cout << "Solving maze" << std::endl;
+    tp.start();
     m.solve();
+    tp.stop();
+    tp.print();
     //m.save_maze();
 
     std::cout << "Saving solution" << std::endl;
     m.save_solution();
-
-    tp.stop();
-    tp.print();
 
     return 0;
 }
